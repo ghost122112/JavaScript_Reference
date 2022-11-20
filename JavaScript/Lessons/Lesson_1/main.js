394a86fc5b7750f46256df47025c78c09d35af8e
Jaloliddin <jalol101.ghost@yandex.com>
2022-11-20 19:14:25 +0500

    .

--- JavaScript/Lessons/Lesson_1/main.js
+++ JavaScript/Lessons/Lesson_1/main.js
@@ -1,3 +1,11 @@
 console.log("Lesson 1: Output"); // Lesson 1: Output
 
 document.getElementById("h1").innerHTML = "Lesson 1: Output";
+
+const googleLink = () => {
+    window.open("https://www.google.com");
+}
+
+addEventListener("click", googleLink, true);
+
+
