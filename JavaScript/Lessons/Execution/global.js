6edfa1ef4dffce7e489272ccc9716af8801c7be8
Jaloliddin <jalol101.ghost@yandex.com>
2022-11-20 20:47:10 +0500

    .

---
+++ JavaScript/Lessons/Execution/global.js
@@ -0,0 +1,34 @@
+"use strict"; // strict mode
+
+// This is global javascript file
+
+const link1 = () => window.open("../Lesson_1/index.html");
+const link2 = () => window.open("../Lesson_2/index.html");
+const link3 = () => window.open("../Lesson_3/index.html");
+const link4 = () => window.open("../Lesson_4/index.html");
+const link5 = () => window.open("../Lesson_5/index.html");
+const link6 = () => window.open("../Lesson_6/index.html");
+const link7 = () => window.open("../Lesson_7/index.html");
+const link8 = () => window.open("../Lesson_8/index.html");
+const link9 = () => window.open("../Lesson_9/index.html");
+const link10 = () => window.open("../Lesson_10/index.html");
+const link11 = () => window.open("../Lesson_11/index.html");
+const link12 = () => window.open("../Lesson_12/index.html");
+const link13 = () => window.open("../Lesson_13/index.html");
+const link14 = () => window.open("../Lesson_14/index.html");
+const link15 = () => window.open("../Lesson_15/index.html");
+const link16 = () => window.open("../Lesson_16/index.html");
+const link17 = () => window.open("../Lesson_17/index.html");
+const link18 = () => window.open("../Lesson_18/index.html");
+const link19 = () => window.open("../Lesson_19/index.html");
+const link20 = () => window.open("../Lesson_20/index.html");
+const link21 = () => window.open("../Lesson_21/index.html");
+const link22 = () => window.open("../Lesson_22/index.html");
+const link23 = () => window.open("../Lesson_23/index.html");
+const link24 = () => window.open("../Lesson_24/index.html");
+const link25 = () => window.open("../Lesson_25/index.html");
+const link26 = () => window.open("../Lesson_26/index.html");
+const link27 = () => window.open("../Lesson_27/index.html");
+const link28 = () => window.open("../Lesson_28/index.html");
+const link29 = () => window.open("../Lesson_29/index.html");
+const link30 = () => window.open("../Lesson_30/index.html");
